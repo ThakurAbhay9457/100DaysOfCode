d6b60cdcd5559d06af71f827ef46ecf289df912b
ThakurAbhay9457 <143515821+ThakurAbhay9457@users.noreply.github.com>
2025-05-15 23:54:21 +0530

    Create RemoveElements.java
    #Leetcode

---
+++ Linkedlist/RemoveElements.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        
+        ListNode newNode = new ListNode(0, head);
+        newNode.next = head;
+        ListNode temp = newNode;
+
+        while(temp.next != null){
+            if(temp.next.val == val){
+                temp.next = temp.next.next;
+            }else{
+                temp = temp.next;
+            }
+        }
+        return newNode.next;
+    }
+}
